d389078b2253514681ce0ea271bcbf71fa85e026
panjiprann <panjipranantya20@gmail.com>
2025-10-08 09:12:13 +0700

    first commit

---
+++ 20230140230-node-server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+    const cors = require('cors');
+    const app = express();
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello from Node.js Server!' });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
