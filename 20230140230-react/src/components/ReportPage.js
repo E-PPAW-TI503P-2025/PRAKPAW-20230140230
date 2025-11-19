c3bfe6811454df3988925b3d744f294b4ef0bc42
panjiprann <panjipranantya20@gmail.com>
2025-11-19 09:58:53 +0700

    p

---
+++ 20230140230-react/src/components/ReportPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ReportPage() {
+  return (
+    <div className="p-10 text-center text-2xl">
+      <h1>Laporan Presensi (Admin)</h1>
+    </div>
+  );
+}
