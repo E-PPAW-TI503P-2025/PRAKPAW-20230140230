6e54ceae10fb6e49ae927af9dceb35ab59a3f6bd
panjiprann <panjipranantya20@gmail.com>
2025-12-02 23:02:37 +0700

    p

--- 20230140230-react/src/App.js
+++ 20230140230-react/src/App.js
@@ -1,22 +1,24 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LoginPage from "./components/LoginPage";
-import RegisterPage from "./components/RegisterPage";
-import DashboardPage from "./components/DashboardPage"; // <-- pastikan file ini ada
-import PresensiPage from "./components/PresensiPage";
-import ReportPage from "./components/ReportPage";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import LoginPage from './components/LoginPage';
+import RegisterPage from './components/RegisterPage';
+import DashboardPage from './components/DashboardPage';
+import Navbar from './components/Navbar';
+import PresensiPage from './components/PresensiPage';
+import ReportPage from './components/ReportPage';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
-        <Route path="/presensi" element={<PresensiPage />} />
-        <Route path="/report" element={<ReportPage />} />
+        <Route path="/dashboard" element={<><Navbar /><DashboardPage /></>} />
+        <Route path="/presensi" element={<><Navbar /><PresensiPage /></>} />
+        <Route path="/reports" element={<><Navbar /><ReportPage /></>} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
