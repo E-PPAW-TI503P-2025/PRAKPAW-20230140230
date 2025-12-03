3e79562933ca72a6173c20a3729b5ed194db5954
panjiprann <panjipranantya20@gmail.com>
2025-12-03 08:26:22 +0700

    p

--- 20230140230-react/src/components/PresensiPage.js
+++ 20230140230-react/src/components/PresensiPage.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 // Tentukan URL API secara terpusat
-const API_URL = "http://localhost:3000/api/presensi"; 
+const API_URL = "http://localhost:3001/api/presensi"; 
 
 export default function PresensiPage() {
   const [message, setMessage] = useState("");
